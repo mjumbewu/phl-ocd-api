88233c9beb59a8ef2064ec6a6e4a5764dcc6c1c4
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2016-03-19 19:18:00 -0400

    Initial commit

---
+++ ocd/data/shapefiles/phila_definitions.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Philadelphia City Council Districts',
+    encoding='iso-8859-1',
+    srid=3702,
+    last_updated=date(2011, 11, 28),
+    name='Philadelphia City Council Districts',
+    singular='Philadelphia City Council District',
+    domain='Philadelphia, PA, USA',
+    authority='Philadelphia City Planning Commission',
+    source_url='https://www.opendataphilly.org/dataset/city-council-districts',
+    start_date=date(2016, 01, 01),
+    name_func=boundaries.clean_attr('DISTRICT'),
+    id_func=boundaries.attr('OBJECTID'),
+)
